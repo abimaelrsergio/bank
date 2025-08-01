6318db0546a117d35cb03ccfd974e4c7360a2436
Abimael Sergio <abimaelr.sergio@gmail.com>
2025-08-01 08:40:57 -0300

    feat: Update audit columns using Spring Data

--- accounts/src/main/java/com/bank/accounts/entity/BaseEntity.java
+++ accounts/src/main/java/com/bank/accounts/entity/BaseEntity.java
@@ -2,24 +2,31 @@ package com.bank.accounts.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.data.annotation.*;
+import org.springframework.data.jpa.domain.support.*;
 
 import java.time.*;
 
+@EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
 @Getter
 @Setter
 @ToString
 public class BaseEntity {
 
+    @CreatedBy
     @Column(updatable = false)
     private String createdBy;
 
+    @CreatedDate
     @Column(updatable = false)
     private LocalDateTime createdAt;
 
+    @LastModifiedDate
     @Column(insertable = false)
     private LocalDateTime updatedAt;
 
+    @LastModifiedBy
     @Column(insertable = false)
     private String updatedBy;
 
