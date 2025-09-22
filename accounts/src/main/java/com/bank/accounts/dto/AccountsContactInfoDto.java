c7f44e7a02cc26b4abbaba707d395972c0152716
Abimael Sergio <abimaelr.sergio@gmail.com>
2025-09-22 08:52:36 -0300

    feat: Refresh configurations at runtime using refresh actuator path

--- accounts/src/main/java/com/bank/accounts/dto/AccountsContactInfoDto.java
+++ accounts/src/main/java/com/bank/accounts/dto/AccountsContactInfoDto.java
@@ -1,9 +1,15 @@
 package com.bank.accounts.dto;
 
+import lombok.*;
 import org.springframework.boot.context.properties.*;
 
 import java.util.*;
 
 @ConfigurationProperties(prefix = "accounts")
-public record AccountsContactInfoDto(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
+@Getter
+@Setter
+public class AccountsContactInfoDto{
+    private String message;
+    private Map<String, String> contactDetails;
+    private List<String> onCallSupport;
 }
